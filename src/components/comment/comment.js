c197b5cf9c51745ed9d9840eba4cc12fbf9b16c8
Shittu Ibrahim <shit2wicw@gmail.com>
2022-01-03 08:31:43 +0000

    feat-Add functionality to add and remove icon

--- src/components/comment/comment.js
+++ src/components/comment/comment.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import ReplyIcon from "@mui/icons-material/Reply";
 import Avatar from "@mui/material/Avatar";
 
-import image from "../../images/avatars/image-amyrobson.png";
+// import image from "../../images/avatars/image-amyrobson.png";
 
 import "./comment.css";
 
-const comment = ({ avatar, name, time, comment }) => {
+const Comment = ({ avatar, name, time, comment }) => {
+  const [upvote, setUpvote] = useState(0);
+
   return (
     <div className="container">
       <div className="leftside">
-        <AddIcon className="icon" />
-        <span className="upvote dark__blue">12</span>
-        <RemoveIcon className="icon" />
+        <AddIcon
+          className="icon"
+          onClick={() => setUpvote((prevState) => prevState + 1)}
+        />
+        <span className="upvote dark__blue">{upvote}</span>
+        <RemoveIcon
+          className="icon"
+          onClick={() => setUpvote((prevState) => prevState - 1)}
+        />
       </div>
       <div className="rightside">
         <div className="rightside__header">
@@ -34,4 +42,4 @@ const comment = ({ avatar, name, time, comment }) => {
   );
 };
 
-export default comment;
+export default Comment;
